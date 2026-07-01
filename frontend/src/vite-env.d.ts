b401ca013b32b86147664450ad5b3586dbbbe173
king <king@ubuntu2026.tailed8a37.ts.net>
2026-07-01 16:33:28 +0000

    Add GHCR publishing and Dify chatbot embed

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,6 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_VERSION: string
+  readonly VITE_DIFY_CHATBOT_TOKEN?: string
+  readonly VITE_DIFY_CHATBOT_BASE_URL?: string
 }
 
 interface ImportMeta {
