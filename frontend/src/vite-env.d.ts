7124e2f3513974754007721e77cf3c806270181c
king <king@ubuntu2026.tailed8a37.ts.net>
2026-07-01 18:02:54 +0000

    Use adaptive Dify embed host

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_APP_VERSION: string
   readonly VITE_DIFY_CHATBOT_TOKEN?: string
   readonly VITE_DIFY_CHATBOT_BASE_URL?: string
+  readonly VITE_DIFY_CHATBOT_HTTPS_BASE_URL?: string
 }
 
 interface ImportMeta {
